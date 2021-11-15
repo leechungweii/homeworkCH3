5e5d643238d78fa2b22f7f02a19f02350fbc1374
leechungweii <tainanwilly@gmail.com>
2021-11-16 00:37:45 +0800

    first commit

---
+++ P17/src/P17/P17.java
@@ -0,0 +1,37 @@
+package P17;
+
+public class P17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1;
+		car1 =new Car();
+		car1.setNumGas(1234,20.5);
+		int number =car1.getNum();
+		double gasoline = car1.getGas();
+		System.out.println("調查樣品車時的知");
+		System.out.println("將車號是"+number+",汽油量為"+gasoline);
+	}
+}
+
+class Car
+{
+	int num;
+	double gas;
+	int getNum()
+	{
+		System.out.println("調查車號");
+		return num;
+	}
+	double getGas()
+	{
+		System.out.println("調查汽油量");
+		return gas;
+	}
+	void setNumGas(int n,double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("將車號設為"+num+",汽油量設為"+gas);
+	}
+}
---
+++ P5/src/P5/P5.java
@@ -0,0 +1,17 @@
+package P5;
+
+public class P5 
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Car car1;
+		car1=new Car();
+	}
+
+}
+
+class Car
+{
+	
+}
